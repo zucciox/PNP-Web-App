f89bfaf1b06e7f5540d5e93c52c85cdb7373eb4d
zuccio <248201657+zucciox@users.noreply.github.com>
2026-05-08 09:41:27 -0400

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/your-repo-name/', // Must match your GitHub repo name
   plugins: [react()],
 })
+
