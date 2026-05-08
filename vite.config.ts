e5fae233d44b20b6c4533953ca811e1d2bed380e
zuccio <248201657+zucciox@users.noreply.github.com>
2026-05-08 10:09:59 -0400

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // Must match your GitHub repo name
   plugins: [react()],
+  base: '/PNP-Web-App/', // ADD THIS LINE
 })
-
