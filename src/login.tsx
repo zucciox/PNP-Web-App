dccd16b35b7571f1608815d7ac45dc04e6f2fc2c
zuccio <248201657+zucciox@users.noreply.github.com>
2026-05-08 09:52:47 -0400

    Update login.tsx

--- src/login.tsx
+++ src/login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Add this
 import { supabase } from './supabaseClient';
-import './app.css';
+import './App.css';
 
 export const LoginPage = () => {
   const [username, setUsername] = useState('');
