0991fb3ef1c57c85f4f5d2b30ed528da9bad259c
zuccio <248201657+zucciox@users.noreply.github.com>
2026-05-08 10:06:28 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// 1. Change createBrowserRouter to createHashRouter
+import { createHashRouter, RouterProvider } from "react-router-dom"; 
 import Home from "./Home";
 import Dashboard from "./dashboard/Dashboard";
 import Login from "./login";
 import AdminPanel from "./adminPanel/AdminPanel";
 
-const router = createBrowserRouter([
+// 2. Update the function call here
+const router = createHashRouter([
     { path: "/", element: <Home /> },
     { path: "/dashboard", element: <Dashboard /> },
     { path: "/login", element: <Login /> },
